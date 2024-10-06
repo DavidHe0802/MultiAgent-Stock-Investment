acc90ad086e208e4ba8f21342d3470af3e94047b
DavidHe0802 <97209947+DavidHe0802@users.noreply.github.com>
2024-10-05 20:53:04 -0400

    Delete Stock Information Digest directory

--- Stock Information Digest/database/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATABASE_PATH = os.path.join(BASE_DIR, 'data', 'stock_data.db')
-STOCK_LIST_PATH = os.path.join(BASE_DIR, 'data', 'stock_list.csv')
